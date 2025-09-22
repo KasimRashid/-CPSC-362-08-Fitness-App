52d783c26c774a8f8bd5b6cd850a36d1ee09d7da
Fabian Gomez <60207011+FabianGomez3@users.noreply.github.com>
2025-09-22 11:25:53 -0700

    Update LoginScript.js

--- LoginScript.js
+++ LoginScript.js
@@ -1,5 +1,5 @@
-function GoToCreateAccout(){
-    window.location.href = "createAccount.html";
+function GoToCreateAccount(){
+    window.location.href = "createaccount.html";
   }
 document.getElementById("loginForm").addEventListener("submit", function(event) {
   event.preventDefault();
@@ -7,19 +7,24 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
   const message = document.getElementById("message");
+  
+  const accounts = JSON.parse(localStorage.getItem("accounts")) || {};
 
 
-
-  if (username === "fitness" && password === "a") {
+  if (username === "Admin" && password === "Admin1") {
     window.location.href = "homePage.html"
     message.style.color = "green";
     message.textContent = " You are in !";
+    window.location.href = "homepage.html";
+  }
+  if (accounts[username] && accounts[username].password === password) {
+    message.style.color = "green";
+    message.textContentn = "You are in !"
+    window.location.href = "homepage.html"
   } else {
-
     message.style.color = "red";
     message.textContent = "Invalid username or password.";
   }
 
-
-
 });
+
