ee649e04a9c4285471ea82f651dfa875d64af14e
Fabian Gomez <60207011+FabianGomez3@users.noreply.github.com>
2025-09-22 11:41:30 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,30 @@
+function GoToCreateAccount(){
+    window.location.href = "createaccount.html";
+  }
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const message = document.getElementById("message");
+  
+  const accounts = JSON.parse(localStorage.getItem("accounts")) || {};
+
+
+  if (username === "Admin" && password === "Admin1") {
+    window.location.href = "homePage.html"
+    message.style.color = "green";
+    message.textContent = " You are in !";
+    window.location.href = "homepage.html";
+  }
+  if (accounts[username] && accounts[username].password === password) {
+    message.style.color = "green";
+    message.textContentn = "You are in !"
+    window.location.href = "homepage.html"
+  } else {
+    message.style.color = "red";
+    message.textContent = "Invalid username or password.";
+  }
+
+});
+
